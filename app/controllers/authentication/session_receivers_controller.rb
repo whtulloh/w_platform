a4ddbf6f03d1258b0950a466a3794c1eccd89123
whtulloh <wh.tulloh@gmail.com>
2013-01-17 16:28:19 +0700

    Update app/controllers/authentication/session_receivers_controller.rb

--- app/controllers/authentication/session_receivers_controller.rb
+++ app/controllers/authentication/session_receivers_controller.rb
@@ -4,6 +4,7 @@ class Authentication::SessionReceiversController < WPlatformController
     session[:user_features] = nil
     if has_complete_params? or (have_session=has_complete_sessions?)
       initialize_user_sessions unless have_session
+      flash[:notice] = "Signed in successfully."
       redirect_to root_path
     else
       get_active_session_or_back_to_platform
