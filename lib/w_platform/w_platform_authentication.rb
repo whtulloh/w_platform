697ef58722c4131c2209e310fc073d637655bea0
whtulloh <wh.tulloh@gmail.com>
2013-01-23 12:09:16 +0700

    Update lib/w_platform/w_platform_authentication.rb
    add username on session

--- lib/w_platform/w_platform_authentication.rb
+++ lib/w_platform/w_platform_authentication.rb
@@ -71,6 +71,7 @@ module WPlatformAuthentication
       user = { 'first_name' => user_data['first_name'],
         'last_name' => user_data['last_name'],
         'email' => user_data['email'],
+        'user' => user_data['login'],
       }
       session[:user_platform] = user
     end
